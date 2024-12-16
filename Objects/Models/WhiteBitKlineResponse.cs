67231bed4f46245255427f8e0886d008de31958c
gurisozen07 <guris.ozen@hexateknoloji.com>
2024-12-16 11:28:21 +0000

    whitebit sup models added

---
+++ Objects/Models/WhiteBitKlineResponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace WhiteBit.Net.Objects.Models
+{
+    public class KlineResponse
+    {
+        [JsonPropertyName("error")]
+        public object? Error { get; set; } 
+
+        [JsonPropertyName("result")]
+        public List<List<object>> Result { get; set; } = new(); 
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; } 
+    }
+
+    public class Kline
+    {
+        public long Timestamp { get; set; }
+        public string Open { get; set; }
+        public string Close { get; set; }
+        public string High { get; set; }
+        public string Low { get; set; }
+        public string Volume { get; set; }
+        public string QuoteVolume { get; set; }
+        public string Symbol { get; set; }
+    }
+}
---
+++ Objects/Models/WhiteBitMarketInfo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace WhiteBit.Net.Objects.Models
+{
+    public class MarketPairInfo
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("stock")]
+        public string Stock { get; set; }
+
+        [JsonPropertyName("money")]
+        public string Money { get; set; }
+
+        [JsonPropertyName("stockPrec")]
+        public string StockPrec { get; set; }
+
+        [JsonPropertyName("moneyPrec")]
+        public string MoneyPrec { get; set; }
+
+        [JsonPropertyName("feePrec")]
+        public string FeePrec { get; set; }
+
+        [JsonPropertyName("makerFee")]
+        public string MakerFee { get; set; }
+
+        [JsonPropertyName("takerFee")]
+        public string TakerFee { get; set; }
+
+        [JsonPropertyName("minAmount")]
+        public string MinAmount { get; set; }
+
+        [JsonPropertyName("minTotal")]
+        public string MinTotal { get; set; }
+
+        [JsonPropertyName("tradesEnabled")]
+        public bool TradesEnabled { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("isCollateral")]
+        public bool IsCollateral { get; set; }
+
+        [JsonPropertyName("maxTotal")]
+        public string MaxTotal { get; set; }
+    }
+}
